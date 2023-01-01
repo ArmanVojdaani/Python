5f77098d5bd962c8c4cb78dc249c699bd0587ac4
ArmanVojdaani <121355681+ArmanVojdaani@users.noreply.github.com>
2023-01-01 23:29:22 +0330

    Update miniproject.py
    3

--- miniproject.py
+++ miniproject.py
@@ -5,4 +5,29 @@ if playing != "yes":
     quit()
 else:
     print(" ok, lets play:) ")
-answer = input("What does cpu stand for?")
+answer = input("What does cpu stand for? ")
+# q1
+if answer.lower() == "central proccessing unit":
+    print("correct")
+else:
+    print("incorrect")
+
+# q2
+answer = input("What does gpu stand for? ")
+if answer == "graphics proccessing unit":
+    print("correct")
+else:
+    print("incorrect")
+
+# q3
+answer = input("What does ram stand for? ")
+if answer == "random access memory":
+    print("correct")
+else:
+    print("incorrect")
+# 4
+answer = input("What does psu stand for? ")
+if answer == "power supply":
+    print("correct")
+else:
+    print("incorrect")

9e9604ee750880de74e81ba1a60edc6a55525817
ArmanVojdaani <121355681+ArmanVojdaani@users.noreply.github.com>
2023-01-09 19:54:30 +0330

    Update miniproject.py
    finished first project

--- miniproject.py
+++ miniproject.py
@@ -1,7 +1,8 @@
 print("Hello Human")
+score = 0
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 else:
     print(" ok, lets play:) ")
@@ -9,25 +10,34 @@ answer = input("What does cpu stand for? ")
 # q1
 if answer.lower() == "central proccessing unit":
     print("correct")
+    score += 1
 else:
     print("incorrect")
 
 # q2
 answer = input("What does gpu stand for? ")
-if answer == "graphics proccessing unit":
+if answer.lower() == "graphics proccessing unit":
     print("correct")
+    score += 1
 else:
     print("incorrect")
 
 # q3
 answer = input("What does ram stand for? ")
-if answer == "random access memory":
+if answer.lower() == "random access memory":
+    score += 1
     print("correct")
 else:
     print("incorrect")
 # 4
 answer = input("What does psu stand for? ")
-if answer == "power supply":
+if answer.lower() == "power supply":
+    score += 1
     print("correct")
 else:
     print("incorrect")
+
+# end of question
+
+print(" you got " + str(score) + " question correct")
+print("equal to " + str(score/4*100) + "percent")

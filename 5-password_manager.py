dbbd94c9043212e99fa7ca4c7ffb74f533934362
ArmanVojdaani <121355681+ArmanVojdaani@users.noreply.github.com>
2023-01-16 20:39:46 +0330

    last mini project

---
+++ 5-password_manager.py
@@ -0,0 +1,49 @@
+from cryptography.fernet import Fernet
+
+''''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
+
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+
+key = load_key()
+fer = Fernet(key)
+
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("user:", user, " | password:",
+                  fer.decrypt(passw.encode()).decode())
+
+
+def add():
+    name = input("account name:  ")
+    pwd = input("password:  ")
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+
+while True:
+    mode = input(
+        "would you like to view your password or add new one? press q to quit. (view/add) ?   ").lower()
+    if mode == "q":
+        break
+    elif mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("invalid mode")
+        continue
